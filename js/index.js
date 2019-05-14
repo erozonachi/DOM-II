7814d0e5b4083e0d1e30bf30299c6f73d86fe4d0
erozonachi <erozonachi.james@gmail.com>
2019-05-14 13:00:28 +0100

    Add mouseover and mouseout event listeners
    Add mouseover event listeners on images, which when trigger should
    make the target 80% gray. Also, add mouseout event listeners on
    images, which when trigger should return the target to it's original
     appearance.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// `mouseover` and `mouseout` listeners on images
+document.querySelectorAll(`img`).forEach(img => {
+  img.addEventListener(`mouseover`, e => {
+    e.stopPropagation();
+    e.target.classList.toggle(`toGray`);
+  });
+  img.addEventListener(`mouseout`, e => {
+    e.stopPropagation();
+    e.target.classList.toggle(`toGray`);
+  });
+});
