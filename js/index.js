8483ed99b40bbedb68a78e447a13af4c9662c8e1
erozonachi <erozonachi.james@gmail.com>
2019-05-14 13:58:44 +0100

    Add keydown and resize event listeners
    Add keydown event listener on the body that will toggle the page
    look and appearance. Add resize event listener to the window that
    will change the font family and size on the body, when the window's
     inner width is less than or equal to 950 pixel.

--- js/index.js
+++ js/index.js
@@ -10,3 +10,20 @@ document.querySelectorAll(`img`).forEach(img => {
     e.target.classList.toggle(`toGray`);
   });
 });
+
+// `keydown` listener on the body
+document.body.addEventListener(`keydown`, e => {
+  e.stopPropagation();
+  document.body.classList.toggle(`pageLook`);
+});
+
+// `resize` event listener on the window
+window.addEventListener(`resize`, e => {
+  e.stopPropagation();
+  if (window.innerWidth <= 950) {
+    document.body.classList.remove(`pageLook`);
+    document.body.classList.add(`awesomeFont`);
+  } else {
+    document.body.classList.remove(`awesomeFont`);
+  }
+});
